486d2c3e6062f370c7be499ddef10d816fc3f85c
michael-stanin <michael.stanin@gmail.com>
2017-06-03 22:35:25 +0300

    Do not write to the configuration file on each stop.
    Write to the configuration file only when we exit the application.
    Also auto start on changed folders only if they are valid and the auto
    start check box is selected.

--- src/runApp.py
+++ src/runApp.py
@@ -8,5 +8,5 @@ from gui.mainWindow import MainWindow
 app = QtWidgets.QApplication(sys.argv)
 playerWindow = MainWindow()
 playerWindow.show()
-app.aboutToQuit.connect(playerWindow.stop)
+app.aboutToQuit.connect(playerWindow.exit)
 sys.exit(app.exec_())
