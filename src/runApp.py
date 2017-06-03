cc317eb3244edb0ef5424d45d333e255247c46cd
michael-stanin <michael.stanin@gmail.com>
2017-06-03 22:35:39 +0300

    Revert "Do not write to the configuration file on each stop."
    This reverts commit 486d2c3e6062f370c7be499ddef10d816fc3f85c.

--- src/runApp.py
+++ src/runApp.py
@@ -8,5 +8,5 @@ from gui.mainWindow import MainWindow
 app = QtWidgets.QApplication(sys.argv)
 playerWindow = MainWindow()
 playerWindow.show()
-app.aboutToQuit.connect(playerWindow.exit)
+app.aboutToQuit.connect(playerWindow.stop)
 sys.exit(app.exec_())
